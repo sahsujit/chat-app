aa85a2d393c2af69e17cab3e3f7f90ea2f44720f
sujit sah <sujit8877123sah@gmail.com>
2024-10-01 20:16:00 +0545

    call

--- src/pages/dashboard/Call.js
+++ src/pages/dashboard/Call.js
@@ -1,8 +1,113 @@
-import React from 'react'
+import {
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  Link,
+} from "@mui/material";
+import { MagnifyingGlass, Phone } from "phosphor-react";
+import React, { useEffect, useState } from "react";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+
+import { useTheme } from "@mui/material/styles";
+import { CallList } from "../../data";
+import { CallLogElement } from "../../components/CallElement";
+import StartCall from "../../sections/main/StartCall";
+
 
 const Call = () => {
+  const theme = useTheme();
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
   return (
-    <div>Call</div>
+    <>
+      <Stack direction="row" sx={{ width: "100%" }}>
+        {/* Left */}
+        <Box
+          sx={{
+            overflowY: "scroll",
+
+            height: "100vh",
+            width: 320,
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+
+            <Stack
+              alignItems={"center"}
+              justifyContent="space-between"
+              direction="row">
+              <Typography variant="h5">Call Log</Typography>
+
+            </Stack>
+
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Stack>
+
+            <Stack
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              direction={"row"}
+            >
+              <Typography variant="subtitle2" sx={{}} component={Link}>
+                Start a conversation
+              </Typography>
+              <IconButton
+              onClick={handleOpenDialog}
+
+              >
+                <Phone style={{ color: theme.palette.primary.main }} />
+              </IconButton>
+            </Stack>
+            <Divider />
+
+            <Stack sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}>
+           
+                <Stack spacing={2.4}>
+                  {CallList.map((el, idx) => {
+                    return <CallLogElement key={idx} {...el} />;
+                  })}
+                </Stack>
+             
+            </Stack>
+
+          </Stack>
+
+        </Box>
+
+      </Stack>
+
+      {openDialog && (
+        <StartCall open={openDialog} handleClose={handleCloseDialog} />
+      )}
+
+    </>
   )
 }
 
